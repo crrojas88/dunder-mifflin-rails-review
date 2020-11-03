a995210997fd0b8fc2dd005cb41f7b8bb7431925
crrojas88 <carlorrojas@gmail.com>
2020-11-02 16:20:27 -0800

    all deliverables met

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+
+    def index
+        if params[:sort]
+            @dogs = Dog.all.sort_by do |dog|
+                dog.employees.size
+            end.reverse
+        else
+            @dogs = Dog.all
+        end
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+
+
 end
